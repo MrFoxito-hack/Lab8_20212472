7db0164bfe779a2328a054e41d06628af0b784f5
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-05-30 19:04:57 -0500

    Initial commit with Spring Boot project

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:25-ea-17-jdk
+VOLUME /tmp
+EXPOSE 8080
+ADD ./target/Lab8_20212472-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/test/java/com/example/lab8_20212472/Lab820212472ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20212472;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820212472ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
